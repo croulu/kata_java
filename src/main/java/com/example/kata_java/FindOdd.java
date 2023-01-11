4f153b59faaddc9bfe32e510ccc6bdf34405769e
Sylvie PONTHUSVIOLLAND <sponthus@gmail.com>
2023-01-11 21:18:04 +0100

    implem in progress

--- src/main/java/com/example/kata_java/FindOdd.java
+++ src/main/java/com/example/kata_java/FindOdd.java
@@ -1,7 +1,20 @@
 package com.example.kata_java;
 
+import java.util.HashMap;
+import java.util.Set;
+
 public class FindOdd {
     public static int findIt(int[] a) {
+
+        HashMap<Integer,Integer> hashmap = new HashMap<Integer,Integer>();
+        for (int j = 0; j < a.length; j++) {
+            hashmap.put(a[j], j);
+        }
+
+        Set<Integer> distinctValues = hashmap.keySet();
+
+        System.out.println(distinctValues);
+
         return 1;
     }
 }
