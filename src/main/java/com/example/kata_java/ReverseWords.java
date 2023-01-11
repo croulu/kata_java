a64a5685258a152c92e5e8ebf48e9741b386305b
Sylvie PONTHUSVIOLLAND <sponthus@gmail.com>
2023-01-11 19:43:36 +0100

    implem reverseWord

--- src/main/java/com/example/kata_java/ReverseWords.java
+++ src/main/java/com/example/kata_java/ReverseWords.java
@@ -1,4 +1,12 @@
 package com.example.kata_java;
 
 public class ReverseWords {
+    public static String reverseWords(String str){
+        String newStr = "";
+        String[] strSplitted = str.split(" ", 0);
+        for (String word : strSplitted) {
+            newStr += word + " ";
+        }
+        return newStr.substring(0, newStr.length() - 1);
+    }
 }
--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -1,3 +1,4 @@
+import com.example.kata_java.ReverseWords;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
