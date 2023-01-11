290e9688abb1cc2f268e2568e9a20c273c79079a
Sylvie PONTHUSVIOLLAND <sponthus@gmail.com>
2023-01-11 19:53:41 +0100

    test ok

--- src/main/java/com/example/kata_java/ReverseWords.java
+++ src/main/java/com/example/kata_java/ReverseWords.java
@@ -4,7 +4,9 @@ public class ReverseWords {
     public static String reverseWords(String str){
         String newStr = "";
         String[] strSplitted = str.split(" ", 0);
-        for (String word : strSplitted) {
+
+        for (int i=strSplitted.length-1; i >= 0 ; i--) {
+            String word = strSplitted[i];
             newStr += word + " ";
         }
         return newStr.substring(0, newStr.length() - 1);
--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -1,16 +1,17 @@
-import com.example.kata_java.ReverseWords;
 import org.junit.jupiter.api.Test;
+
+import static com.example.kata_java.ReverseWords.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 class SolutionTest {
     @Test
     void sampleTests() {
-        assertEquals("world! hello", ReverseWords.reverseWords("hello world!"));
-        assertEquals("this like speak doesn't yoda", ReverseWords.reverseWords("yoda doesn't speak like this"));
-        assertEquals("foobar", ReverseWords.reverseWords("foobar"));
-        assertEquals("editor kata", ReverseWords.reverseWords("kata editor"));
-        assertEquals("boat your row row row", ReverseWords.reverseWords("row row row your boat"));
-        assertEquals("", ReverseWords.reverseWords(""));
+        assertEquals("world! hello", reverseWords("hello world!"));
+        assertEquals("this like speak doesn't yoda", reverseWords("yoda doesn't speak like this"));
+        assertEquals("foobar", reverseWords("foobar"));
+        assertEquals("editor kata", reverseWords("kata editor"));
+        assertEquals("boat your row row row", reverseWords("row row row your boat"));
+        assertEquals("", reverseWords(""));
     }
 }
