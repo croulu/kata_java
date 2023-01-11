74e4889411f847b0fcd82404c6ba05419870d920
Sylvie PONTHUSVIOLLAND <sponthus@gmail.com>
2023-01-11 20:35:58 +0100

    test ok

--- src/main/java/com/example/kata_java/StringEndsWith.java
+++ src/main/java/com/example/kata_java/StringEndsWith.java
@@ -3,6 +3,11 @@ package com.example.kata_java;
 public class StringEndsWith {
 
     public static boolean stringEndsWith(String str, String ending) {
-        return false;
+        int posEndingInStr = str.lastIndexOf(ending);
+        if (posEndingInStr == -1) {
+            return false;
+        } else {
+            return posEndingInStr == str.length() - ending.length() ? true : false;
+        }
     }
 }
